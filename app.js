ea3c79975fb59e9e63ffe4d430813568d058195d
Zachary Carreon <z_carreon@fanshaweonline.ca>
2018-01-29 13:26:15 -0500

    added grunt

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express'); // just like an include or require with php
-const app = express(); // create an inctance of our application via simpleExample
+const app = express(); // create an inctance of our application via simpleExpress
+const io = require('socket.io')();
 
 app.use(express.static('public'));
 
@@ -8,6 +9,17 @@ app.use(require('./routes/index'));
 app.use(require('./routes/contact'));
 app.use(require('./routes/portfolio'));
 
-app.listen(3000, () => {
-  console.log('app running on port 3000!');
+const server = app.listen(3000, () => {
+  console.log('App running on port 3000.');
+});
+
+io.attach(server);
+
+io.on('connection', () => {
+  console.log('A user has connected.');
+  io.emit('connectMsg', { for: 'everyone', msg : '${socket.id} is here.'});
+
+  socket.on('disconnect', () => {
+    console.log('A user has disconnected.');
+  });
 });
